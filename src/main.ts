65cfe50c7ff36a023dafbb60264c6839e5d266ad
PeytenBevill <redsage00@gmail.com>
2024-09-22 14:59:43 -0500

    added cors policy

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,15 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Enable CORS for origin on port 5137, allowing GET, POST, PUT methods
+  app.enableCors({
+    origin: 'http://localhost:5137',
+    methods: ['GET', 'POST', 'PUT'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true,
+  });
+
   await app.listen(3000);
 }
 bootstrap();
