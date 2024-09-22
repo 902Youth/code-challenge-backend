06b377bebe646d7e374c06d337e4e03a77628337
PeytenBevill <redsage00@gmail.com>
2024-09-22 15:04:40 -0500

    had a typo

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
 
   // Enable CORS for origin on port 5137, allowing GET, POST, PUT methods
   app.enableCors({
-    origin: 'http://localhost:5137',
+    origin: 'http://localhost:5173',
     methods: ['GET', 'POST', 'PUT'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true,
